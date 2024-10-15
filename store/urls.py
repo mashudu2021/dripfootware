af1d46b2d3b17da934fb3007f09840c321d29b3a
Mashudu Malukwe <86242460+mashudu2021@users.noreply.github.com>
2024-10-15 21:28:51 +0200

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('Product/<int:pk>', views.product, name='product'),
+    path('about/', views.about, name='about'),
 ]
